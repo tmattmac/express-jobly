b70111566e2f3114732f3bfdb8de91c1d54c8b07
Trey McDeane <tmcdeane@gmail.com>
2020-11-17 20:04:33 -0500

    added ON UPDATE trigger to jobs table

--- schema.sql
+++ schema.sql
@@ -1,23 +1,23 @@
-CREATE TABLE companies {
+CREATE TABLE companies (
     handle TEXT PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     num_employees INTEGER,
     description TEXT,
     logo_url TEXT
-};
+);
 
-CREATE TABLE jobs {
+CREATE TABLE jobs (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     salary FLOAT NOT NULL,
     equity FLOAT NOT NULL,
     date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    company_handle TEXT REFERENCES companies ON DELETE CASCADE,
+    company_handle TEXT REFERENCES companies ON DELETE CASCADE ON UPDATE CASCADE,
     CHECK (salary > 0),
     CHECK (equity >= 0 AND equity <= 1)
-};
+);
 
-CREATE TABLE users {
+CREATE TABLE users (
     username TEXT PRIMARY KEY,
     password TEXT NOT NULL,
     first_name TEXT NOT NULL,
@@ -25,4 +25,4 @@ CREATE TABLE users {
     email TEXT UNIQUE NOT NULL,
     photo_url TEXT,
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
-};
+);
